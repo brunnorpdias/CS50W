9cc28965bf9385b79e056285190aec0416b009a8
brunnorpdias <brunnorpdias@gmail.com>
2020-08-24 00:01:03 -0300

    Project mainly done!

--- project_1/wiki/encyclopedia/urls.py
+++ project_1/wiki/encyclopedia/urls.py
@@ -4,6 +4,12 @@ from . import views
 app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:page>", views.getpage, name="get_page"),
+    path("wiki/<str:page>", views.getpage, name="getpage"),
+    path("wiki/", views.getpage, name="wiki"),
     path("search/", views.search, name="search"),
+    path("newpage/", views.newpage, name="newpage"),
+    path("random/", views.random, name="randompage"),
+    path("edit/<str:page>", views.edit, name="editpage"),
+    path("edit/", views.edit, name="edit"),
+    path("save/", views.save, name="save"),
 ]
--- project_1/wiki/encyclopedia/views.py
+++ project_1/wiki/encyclopedia/views.py
@@ -1,38 +1,47 @@
-from django.core.files.base import ContentFile
-from django.core.files.storage import default_storage
 from django.shortcuts import render
 from . import util
 from django import forms
+from django.urls import reverse
+from django.http import HttpRequest
+from django.http import HttpResponseRedirect
 import markdown2
+import numpy as np
+
 
 
 class Search(forms.Form):
-    query = forms.CharField(label="Search")
+    q = forms.CharField(label="Search")
+
+class NewPage(forms.Form):
+    name = forms.CharField(label="Page Name")
+    text = forms.CharField(widget=forms.Textarea)
+
+class EditPage(forms.Form):
+    name = forms.CharField(label="Page Name")
+    text = forms.CharField(widget=forms.Textarea)
+
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", 
-        {"entries": util.list_entries(), "form":Search()}
+        {"entries": util.list_entries(), "sform":Search()}
     )
 
 def getpage(request, page):
     if util.get_entry(page) == None:
         return render(request, "encyclopedia/page.html", 
-        {"content":"<h1>Error(404): Page not found</h1>", "form":Search()}
+        {"content":"<h1>Error(404): Page not found</h1>", "sform":Search()}
         )
     return render(request, "encyclopedia/page.html",
-    {"content":markdown2.markdown(util.get_entry(page)), "page":page, "form":Search()}
+    {"content":markdown2.markdown(util.get_entry(page)), "page":page, "sform":Search()}
     )
 
 def search(request):
-    query = request.GET.urlencode().split("=")
+    query = request.GET.get("q", "")
     searched = []
-    for i in query:
-        if i is not "query":
-            query = i
     for i in util.list_entries():
         if i.lower() == query:
-            return getpage(request, query)
+            return HttpResponseRedirect(reverse("wiki:wiki")+i)
         else:
             if i.lower().find(query) is not -1:
                 searched.append(i)
@@ -42,3 +51,35 @@ def search(request):
         return render(request, "encyclopedia/search.html",
         {"results":searched}
         )
+
+def newpage(request):
+    if request.method == "POST":
+        form = NewPage(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            content = form.cleaned_data["text"]
+            util.save_entry(name, content)
+    return render(request, "encyclopedia/newpage.html",
+    {"nform":NewPage()}
+    )
+    
+def random(request):
+    size = len(util.list_entries())
+    item = np.random.randint(1, size) - 1
+    entries = util.list_entries()[:]
+    page = entries[item]
+    return getpage(request, page)
+
+def edit(request, page):
+    return render(request, "encyclopedia/edit.html",
+    {"page":page, "eform":EditPage({'name':page, 'text':util.get_entry(page)}), "sform":Search()}
+    )
+
+def save(request):
+    if request.method == "POST":
+        form = EditPage(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            content = form.cleaned_data["text"]
+            util.save_entry(name, content)
+            return HttpResponseRedirect(reverse("wiki:wiki")+name)
